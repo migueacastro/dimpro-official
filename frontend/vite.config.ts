ff60d12acc93792e61acc815794232f5566ee683
migueacastro <miguelcastropadron@gmail.com>
2025-06-19 21:31:56 -0400

    make allowed hosts check domain automatically

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,24 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { loadEnv,defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const URL = `${env.DOMAIN ?? 'localhost'}`;
+
+return {
+
   plugins: [sveltekit(), purgeCss()],
   server: {
-    allowedHosts:['castroworks.lat'],
+    allowedHosts:[URL],
     host: "0.0.0.0",
     port: 3000,
   },
   preview: {
-    allowedHosts:['castroworks.lat'],
+    allowedHosts:[URL],
     host: "0.0.0.0",
     port: 3000,
   },
+}
 });
